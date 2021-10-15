7e794ce6ec927b249fa06ddcc4abd429d55f63d1
Travis Harvey <txh120530@gmail.com>
2021-10-14 21:52:45 -0500

    Finished Bulk of Work

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+
+import DecimalInput from './components/DecimalInput';
+import BinaryInput from './components/BinaryInput';
+
 
 function App() {
+  const [binary, setBinary] = useState('');
+  const [decimal, setDecimal] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  // const handleError(data) {
+  //   console.log(data);
+  // }
+
+  const handleError = (index) => {
+    console.log(index);
+    setErrorMessage(index);
+  };
+
+  const handleBinary = (value) => {
+    const decimal = parseInt(value, 2);
+    setBinary(value);
+    setDecimal(decimal);
+  };
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-center">Binary-Decimal Converter</h1>
+      <div className="error text-center h-2 block">{errorMessage && <span style={{ color: 'red' }}>{errorMessage}</span>}</div>
+
+      <div className="block md:flex justify-around items-center border p-12 max-w-3xl mx-auto">
+
+      <BinaryInput handleBinary={handleBinary} handleError={handleError} />
+      <DecimalInput decimalValue={decimal} />
+      </div>
+
     </div>
   );
 }
---
+++ src/components/BinaryInput.js
@@ -0,0 +1,36 @@
+import {useState, useEffect} from 'react';
+
+
+function BinaryInput(props) {
+  
+  const [value, setValue] = useState('');
+  const onChange = (event) => {
+    setValue(event.target.value);  
+  };
+
+     useEffect(() => {
+		if (value.match(/^[0-1]+$/g) === null) {
+	      props.handleError("Enter either 0 or 1 and No more than 8 digits");
+	  	} else{
+	      props.handleError('');
+	  	}
+	  	props.handleBinary(value);
+	}, [value]);
+
+
+
+  return (
+    <div className="binaryInput">
+      <label  className="block" htmlFor="input__binary">Binary</label>
+      <input 
+      	value={value} 
+      	onChange={onChange} 
+      	id="input__binary" 
+      	className="border"
+      	type="text"
+      />
+    </div>
+  );
+}
+
+export default BinaryInput;
